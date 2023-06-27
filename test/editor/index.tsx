55167a78f589c3355197d40db4e9f807e10de64a
ClearLuvMoki <2893096286@qq.com>
2023-06-28 00:29:43 +0800

    update: editor preview

--- test/editor/index.tsx
+++ test/editor/index.tsx
@@ -1,46 +1,49 @@
+import "./index.less"
 import {
   EditorRender,
 } from 'moki-editor';
 import type {Editor} from "moki-editor"
-import React from 'react';
+import React, {useEffect, useTransition} from 'react';
 import {useState} from "react"
 import {content} from "./content";
 import ReactJson from 'react-json-view'
 
 
 const EditorPage = () => {
+  const [isPending, startTransition] = useTransition()
   const [editor, setEditor] = useState<Editor>(null);
   const [state, setState] = useState({
     jsonValue: {},
-    htmlValue: "",
-    textValue: ""
+    htmlValue: content,
   })
 
   return (
-    <div style={{width: "100%"}}>
+    <div className={"EditorPage"}>
       <EditorRender
         ref={setEditor}
         content={content}
         onUpdate={({editor}) => {
-          console.log(editor?.getJSON(), 'editor?.getJSON()')
-          setState({
-            htmlValue: editor?.getHTML(),
-            jsonValue: editor?.getJSON(),
-            textValue: editor?.getText(),
+          startTransition(() => {
+            setState({
+              htmlValue: editor?.getHTML(),
+              jsonValue: editor?.getJSON(),
+            })
           })
         }}
       />
-      <div>
-        <span>输出HTML:</span><span>{JSON.stringify(state?.htmlValue)}</span>
-      </div>
-      <div>
-        <span>输出JSON:</span>
-        <ReactJson
-          src={state?.jsonValue}
-        />
-      </div>
-      <div>
-        <span>输出Text:</span><span>{JSON.stringify(state?.textValue)}</span>
+      <div className={"EditorPage-Segment"}/>
+      <div style={{display: "flex", gap: 10}}>
+        <div className={"EditorPage-ResWrapper"} style={{background: "rgba(230, 247,255, .7)"}}>
+          <h3>Output HTML:</h3>
+          <span>{JSON.stringify(state?.htmlValue)}</span>
+        </div>
+        <div className={"EditorPage-ResWrapper"} style={{background: "rgba(18, 161,130, .2)"}}>
+          <h3>Output JSON:</h3>
+          <ReactJson
+            collapsed={false}
+            src={JSON.parse(JSON.stringify(state?.jsonValue || {}))}
+          />
+        </div>
       </div>
     </div>
   )
